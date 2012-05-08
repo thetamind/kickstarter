6df962d7faf9c42a84ceb610c0a237ba1f2c1268
Matthew Boehlig <thetamind@gmail.com>
2012-05-08 16:34:11 -0500

    Tag real networking specs as slow

--- spec/kickstarter_spec.rb
+++ spec/kickstarter_spec.rb
@@ -6,18 +6,20 @@ describe Kickstarter do
     Kickstarter::Categories.length.should > 0
   end
   
-  PPP = 15 # projects per page
-  
-  it "should list 1 page from recommended projects" do
-    WebMock.disable!
-    projects = Kickstarter.by_list(:recommended, :pages => 1)
-    projects.count.should == PPP
-  end
-  
-  it "should list 2 pages from recommended projects" do
-    WebMock.disable!
-    projects = Kickstarter.by_list(:recommended, :pages => 2)
-    projects.count.should == 2 * PPP
+  context "with real networking", slow: true do
+    PPP = 15 # projects per page
+    
+    it "should list 1 page from recommended projects" do
+      WebMock.disable!
+      projects = Kickstarter.by_list(:recommended, :pages => 1)
+      projects.count.should == PPP
+    end
+    
+    it "should list 2 pages from recommended projects" do
+      WebMock.disable!
+      projects = Kickstarter.by_list(:recommended, :pages => 2)
+      projects.count.should == 2 * PPP
+    end
   end
 
   describe "#by_handle" do
