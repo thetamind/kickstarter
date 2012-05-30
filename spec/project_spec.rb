b074de1f84b3e135570876b32ed81ef78d84ee34
Matthew Boehlig <thetamind@gmail.com>
2012-05-30 11:38:42 -0500

    Add finished project page dump
     * Finshed project have slightly different markup

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -4,14 +4,29 @@ require 'kickstarter'
 describe Kickstarter::Project do
   PROJECT_URL = "#{Kickstarter::BASE_URL}/projects/597507018/pebble-e-paper-watch"
 
-  before(:each) do
-    WebMock.enable!
-    stub_request(:get, PROJECT_URL).
-      to_return( File.new("./spec/project.txt").read )
+  context "when in progress" do
+    before(:each) do
+      WebMock.enable!
+      stub_request(:get, PROJECT_URL).
+        to_return( File.new("./spec/project.txt").read )
+    end
+
+    subject { Kickstarter::Project.new(PROJECT_URL) }
+
+    its(:video_poster_url) { should == "http://s3.amazonaws.com/ksr/projects/111694/photo-full.jpg?1334081632" }
+    its(:video_url) { should == "http://s3.amazonaws.com/ksr/projects/111694/video-96723-h264_high.mp4" }
   end
 
-  subject { Kickstarter::Project.new(PROJECT_URL) }
+  context "when finished" do
+    before(:each) do
+      WebMock.enable!
+      stub_request(:get, PROJECT_URL).
+        to_return( File.new("./spec/finished_project.txt").read )
+    end
 
-   its(:video_poster_url) { should == "http://s3.amazonaws.com/ksr/projects/111694/photo-full.jpg?1334081632" }
-   its(:video_url) { should == "http://s3.amazonaws.com/ksr/projects/111694/video-96723-h264_high.mp4" }
+    subject { Kickstarter::Project.new(PROJECT_URL) }
+
+    its(:video_poster_url) { should == "http://s3.amazonaws.com/ksr/projects/111694/photo-full.jpg?1334081632" }
+    its(:video_url) { should == "http://s3.amazonaws.com/ksr/projects/111694/video-108947-h264_high.mp4" }
+  end
 end
